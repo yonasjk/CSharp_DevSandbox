2f121ba9514bd238e3663323894600913aac5596
Yonas Keflemariam <yonas@sphynxsofware.com>
2017-03-20 15:28:49 -0400

    Initial Commit

---
+++ Example_CalculateSimpleInterest/Example_CalculateSimpleInterest/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+* C# Program to Calculate Simple Interest
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_CalculateSimpleInterest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int year;
+            double princamt, rate, interest, total_amt;
+
+            Console.Write("Enter The Loan Amount : ");
+            princamt = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter The Number of Years : ");
+            year = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter the Rate Of Interest : ");
+            rate = Convert.ToDouble(Console.ReadLine());
+
+            interest = princamt * year * rate / 100;
+            total_amt = princamt + interest;
+
+            Console.WriteLine("Total Amount : {0}", total_amt);
+
+            Console.ReadLine();
+
+       }
+
+    }
+
+}
---
+++ Example_DiagonalMatrix/Example_DiagonalMatrix/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_DiagonalMatrix
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[,] matA = new int[10, 10];
+            bool IsDiag = false;
+
+            Console.WriteLine("\nFor Diagonal Matrix Same no. of row and column required.");
+            Console.Write("\nEnter the no. of row. ");
+            int row = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the no. of col. ");
+            int col = Convert.ToInt32(Console.ReadLine());
+
+            // enter the element of the matrix    
+            Console.WriteLine("\nEnter the element");
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= col; j++)
+                {
+                    Console.Write("Matrix A[{0},{1}]:", i, j);
+                    matA[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            // Print the matrix    
+
+            Console.WriteLine("\nMatrix A: ");
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= col; j++)
+                {
+                    Console.Write("{0} ", matA[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            // Check weather the matrix is diagonal or not    
+
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= col; j++)
+                {
+                    if ((i != j) && (matA[i, j] == 0))
+                    {
+                        IsDiag = true;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            if (IsDiag == true)
+            {
+                Console.WriteLine("Diagonal matrix");
+            }
+            else
+            {
+                Console.WriteLine("Not a Diagonal matrix");
+            }
+        }
+    }
+
+}
